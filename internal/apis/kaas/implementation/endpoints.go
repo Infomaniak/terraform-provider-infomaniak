1f54bd25ab51d85e91e83641d13ca74dd8ec2cc0
Tristan Smagghe <yyewolf@gmail.com>
2025-06-30 10:17:51 +0200

    Revert "feat(kaas): api server params"

--- internal/apis/kaas/implementation/endpoints.go
+++ internal/apis/kaas/implementation/endpoints.go
@@ -10,6 +10,4 @@ var (
 
 	EndpointInstancePools = "/1/public_clouds/{public_cloud_id}/projects/{public_cloud_project_id}/kaas/{kaas_id}/instance_pools"
 	EndpointInstancePool  = "/1/public_clouds/{public_cloud_id}/projects/{public_cloud_project_id}/kaas/{kaas_id}/instance_pools/{kaas_instance_pool_id}"
-
-	EndpointApiserver = "/1/public_clouds/{public_cloud_id}/projects/{public_cloud_project_id}/kaas/{kaas_id}/apiserver"
 )
