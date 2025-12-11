e016d9b66d52911a26f2b888ed41242e9a5b5521
Thomas Betrix <thomas.betrix@infomaniak.com>
2025-12-11 10:10:19 +0100

    feat: add effective configuration to dbaas datasource (#86)
    * feat: add effective configuration to dbaas datasource
    
    Signed-off-by: Thomas Betrix <thomas.betrix@infomaniak.com>
    
    * chore: trigger rebuild
    
    Signed-off-by: Thomas Betrix <thomas.betrix@infomaniak.com>
    
    * chore: reuse refreshEffectiveConfiguration instead of duplicating it
    
    Signed-off-by: Thomas Betrix <thomas.betrix@infomaniak.com>
    
    * feat: add more dbaas datasource tests
    
    Signed-off-by: Thomas Betrix <thomas.betrix@infomaniak.com>
    
    * chore: update dbaas datasource documentation
    
    Signed-off-by: Thomas Betrix <thomas.betrix@infomaniak.com>
    
    ---------
    
    Signed-off-by: Thomas Betrix <thomas.betrix@infomaniak.com>

--- internal/services/dbaas/dbaas_data_source_schema.go
+++ internal/services/dbaas/dbaas_data_source_schema.go
@@ -65,6 +65,10 @@ func getDbaasDataSourceSchema() schema.Schema {
 				Computed:            true,
 				MarkdownDescription: "DbaaS kubernetes name",
 			},
+			"effective_configuration": schema.DynamicAttribute{
+				Computed:            true,
+				MarkdownDescription: "The effective database configuration settings",
+			},
 		},
 		MarkdownDescription: "The dbaas data source allows the user to manage a dbaas project",
 	}
---
+++ internal/services/dbaas/tests/schema/data_source_dbaas_cant_specify_ca.tf
@@ -0,0 +1,36 @@
+terraform {
+  required_version = ">= 1.5"
+
+  required_providers {
+    infomaniak = {
+      source  = "Infomaniak/infomaniak"
+      version = "~> 1.0"
+    }
+  }
+}
+
+provider "infomaniak" {
+  token = "fake-token"
+}
+
+resource "infomaniak_dbaas" "db" {
+  public_cloud_id         = 42
+  public_cloud_project_id = 54
+
+  name    = "test"
+  region  = "dc5-a"
+  type    = "mysql"
+  version = "8.0"
+  allowed_cidrs = [
+    "0.0.0.0/0"
+  ]
+  pack_name = "essential-1"
+}
+
+
+data "infomaniak_dbaas" "db" {
+  public_cloud_id         = 42
+  public_cloud_project_id = 54
+  id                      = infomaniak_dbaas.db.id
+  ca                      = "rupityghldjhg"
+}
